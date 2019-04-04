40396ce45dc0dc107eb06a8a5a77d1f474a89535
alminChan <fir512@126.com>
2019-04-04 14:34:52 +0800

    在YZS的项目新增一个demo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# yzs_经历的所有项目集合以及代码整理
+# 经历的所有项目集合以及代码整理
+
+涉及到vue滚动加载，不是template模板使用 /滚动加载VUE</hr>
+涉及到小程序滚动楼层 /srolrll 使用的是scrow-into-view</hr>
---
+++ mescrollDemo/src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+// import FastClick from 'fastclick'
+import store from './store/store'
+import router from './router'
+import App from './App'
+import 'vue-ydui/dist/ydui.flexible'
+import 'vue-ydui/dist/ydui.base.css'
+import axios from 'axios'
+import QS from 'qs'
+// FastClick.attach(document.body)
+Vue.config.productionTip = false
+Vue.prototype.axios = axios
+Vue.prototype.qs = QS
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: {
+    App
+  }
+})
